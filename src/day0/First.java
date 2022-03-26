6670205173c1a632d145bde3de6cbcbc445093a9
fatihkarakus <fkarakus97@gmail.com>
2022-03-26 16:12:18 -0400

    wrote the code for first class

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,7 +4,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("java");
         System.out.println("working ....");
-
+//yani ki...
     }
 
     //extra line 1
